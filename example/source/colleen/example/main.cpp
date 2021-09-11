e957b98a487a40c1ecb120b6654be23590e69c5d
Tobias Fuchs <fuchstobs@gmail.com>
2021-09-11 16:51:30 +0200

    adds first example and generator - default arguments only

--- example/source/colleen/example/main.cpp
+++ example/source/colleen/example/main.cpp
@@ -2,19 +2,12 @@
 // Created by Tobias Fuchs on 02.01.21.
 //
 
-#include <colleen/colleen.h>
+#include <config/network/hostname.h>
+#include <config/network/port.h>
 #include <iostream>
 
 int main(int argc, char **argv) {
-    Colleen::setExecutableName("unit_tests");
-    Colleen::addArgument<int>({"test", 't'})
-        .setDescription("Optional argument")
-        .setDefault(42);
-    Colleen::addArgument<int>("testArgument0")
-        .setDescription("Test argument at position 0")
-        .setDefault(42);
-
-    std::cout << Colleen::getHelpString() << std::endl;
-
+    std::cout << config::network::hostname.get() << std::endl;
+    std::cout << config::network::port.get() << std::endl;
     return 0;
 }
